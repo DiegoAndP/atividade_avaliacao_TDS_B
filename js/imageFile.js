648542cffbe2751f6be2d37d61af9eabe19cc7b3
Diego Andrade <diegoandrade.xb@gmail.com>
2025-06-12 21:37:26 -0300

    Agora é possível carregar arquivos pelo navegador.

--- js/imageFile.js
+++ js/imageFile.js
@@ -2,12 +2,19 @@ import { context } from "../script.js"
 export { importImage }
 
 function importImage() {
-    const imagePath = document.querySelector('[data-file="file"]').value
-    const image = new Image()
-    image.src = imagePath
-    image.onload = _ => {
-        if (context) {
-            context.drawImage(image, 0, 0)
-        }
-    }
+    const fileInput = document.querySelector('[data-file="file"]');
+    const file = fileInput.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        const image = new Image();
+        image.onload = function() {
+            if (context) {
+                context.drawImage(image, 0, 0);
+            }
+        };
+        image.src = event.target.result;
+    };
+    reader.readAsDataURL(file);
 }
